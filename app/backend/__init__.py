f4c49ee851359ab0555e4d53933d47e281751e78
Diego Pereira <diegop@pm.me>
2023-09-06 01:13:06 -0300

    feat: login with google oauth

--- app/backend/__init__.py
+++ app/backend/__init__.py
@@ -1,38 +1,62 @@
 import aiohttp
 import datetime
+import flask
+import json
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm, CSRFProtect
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 
-def create_app():
+def load_credentials(file_path):
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as secret:
+            data = json.load(secret)
+            return {
+                "GOOGLE_CONFIG":  { k.upper(): v for k, v in data["installed"].items() }
+            }
+    else:
+        return {}
+
+
+def create_app(app_endpoint='http://localhost', port=5000):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         API_BASE='http://localhost:3000',
+        PORT=port,
+        GOOGLE_CONFIG_FILE="./client_secret.json",
+        GOOGLE_REDIRECT_URI=f'{app_endpoint}:{port}',
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres:db712bccc14d602212c928a39ba7e23d@localhost:5432/yourtube_app',
+        SESSION_TYPE='sqlalchemy',
     )
+
+    db = SQLAlchemy(app)
+
+    app.config.from_mapping(SESSION_SQLALCHEMY=db)
     app.config.from_prefixed_env(prefix='YOURTUBE')
+    app.config.from_mapping(**load_credentials(app.config['GOOGLE_CONFIG_FILE']))
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    bootstrap = Bootstrap5(app)
+    csrf = CSRFProtect(app)
+    Session(app)
 
-    return app
+    return app, db, bootstrap, csrf
 
-app = create_app()
-bootstrap = Bootstrap5(app)
-csrf = CSRFProtect(app)
+app, db, booetstrap, csrf = create_app()
 
-def today():
-    return datetime.datetime.today().strftime("%Y-%m-%d") 
+def published_since(days=2):
+    since = datetime.datetime.today() - datetime.timedelta(days=2)
+
+    return since.strftime("%Y-%m-%d")  
 
 class FilterForm(FlaskForm):
     min_duration = StringField('Minimum Duration', default='00:05:00')
     max_duration = StringField('Maximum Duration', default='00:15:00')
-    published_since = StringField('Published Since', default=today)
+    published_since = StringField('Published Since', default=published_since)
     submit_field = SubmitField('Filter')
 
 async def fetch_videos(min_duration = None, max_duration = None, published_since = None):
@@ -56,9 +80,87 @@ async def fetch_videos(min_duration = None, max_duration = None, published_since
 
 @app.route('/', methods=['GET', 'POST'])
 async def root():
+    if not is_logged_in():
+        return redirect('/login')
+
     form = FilterForm()
-    if form.validate_on_submit():
-        print(f"{form.min_duration.data} / {form.max_duration.data} / {form.published_since.data}")
     videos = await fetch_videos(
         form.min_duration.data, form.max_duration.data, form.published_since.data)
     return render_template('index.html', form=form, videos=videos)
+
+
+def redirect_uri():
+    return f'{app.config["GOOGLE_REDIRECT_URI"]}/auth-code'
+
+def is_logged_in():
+    try:
+        print(session.get('expires_at', ''))
+        session_expiration = datetime.datetime.strptime(session.get('expires_at', ''), '%Y-%m-%dT%H:%M:%S')
+
+        print(datetime.datetime.utcnow() ,  session_expiration)
+        return datetime.datetime.utcnow() < session_expiration
+    except ValueError:
+        print('VALUE ERROR')
+        return False
+
+@app.route('/login')
+async def login():
+
+    if is_logged_in():
+        return redirect('/')
+
+    config = app.config['GOOGLE_CONFIG']
+    auth_uri = config['AUTH_URI']
+    client_id = config['CLIENT_ID']
+    scopes = " ".join([
+        "https://www.googleapis.com/auth/youtube.readonly",
+        "https://www.googleapis.com/auth/userinfo.profile",
+    ])
+    google_redirect = redirect_uri()
+
+    uri = f'{auth_uri}?client_id={client_id}&redirect_uri={google_redirect}&scope={scopes}&response_type=code'
+
+    return redirect(uri)
+
+@app.route('/auth-code')
+async def auth_code():
+    timestamp = datetime.datetime.utcnow()
+    config = app.config['GOOGLE_CONFIG']
+    token_uri = config['TOKEN_URI']
+    token_params = {
+        'code': request.args.get('code'),
+        'client_id': config['CLIENT_ID'],
+        'client_secret': config['CLIENT_SECRET'],
+        'redirect_uri': redirect_uri(),
+        'grant_type': 'authorization_code',
+    }
+
+    data = aiohttp.FormData(token_params)
+
+    async with aiohttp.ClientSession() as session:
+        headers = {}
+        async with session.post(token_uri, data=data) as response:
+            token_data = await response.json()
+            flask.session['token'] = token_data
+            headers['Authorization'] = f'Bearer {token_data["access_token"]}'
+
+        if token_data:
+            flask.session['expires_at'] = (
+                timestamp + datetime.timedelta(seconds=token_data['expires_in'])
+            ).strftime('%Y-%m-%dT%H:%M:%S')
+            async with session.get('https://www.googleapis.com/oauth2/v1/userinfo', headers=headers) as response:
+                user_info = await response.json()
+                flask.session['user_info'] = user_info
+                return redirect('/')
+        else:
+            return redirect('/login')
+
+
+@app.route('/user-info')
+async def user_info():
+    return {
+            "info": flask.session['user_info'],
+            "token": flask.session['token'],
+            "expires_at": flask.session['expires_at']
+    }
+
