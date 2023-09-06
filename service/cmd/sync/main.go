7c7affa4f8a92c77df35675483cd2311030bc85e
Diego Pereira <diegop@pm.me>
2023-09-06 19:56:43 -0300

    refactor: remove db password

--- service/cmd/sync/main.go
+++ service/cmd/sync/main.go
@@ -3,5 +3,6 @@ package main
 import "yourtube.51390.cloud/yourtube"
 
 func main() {
+    yourtube.LoadEnv()
     yourtube.Sync("", "", "")
 }
---
+++ service/yourtube/common.go
@@ -0,0 +1,15 @@
+package yourtube
+
+import (
+    "fmt"
+
+    "github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        fmt.Printf("Failed to load .env file: %s\n", err)
+    }
+}
+
