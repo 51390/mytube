f53e8e53017f0eee61e302093b8291bfb2ab663a
Diego Pereira <diegop@pm.me>
2023-09-24 20:59:46 -0300

    feat: allow ssl if server requires it

--- service/mytube/db.go
+++ service/mytube/db.go
@@ -12,7 +12,7 @@ func InitDb() (db *gorm.DB, err error) {
 	password := os.Getenv("POSTGRES_PASSWORD")
 	user := os.Getenv("POSTGRES_USER")
 	host := os.Getenv("POSTGRES_HOST")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=mytube_service port=5432 sslmode=disable", host, user, password)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=mytube_service port=5432 sslmode=allow", host, user, password)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	return
 }
