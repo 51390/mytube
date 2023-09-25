4b350f848024a943dc355b45606f83f42fec48c2
Diego Pereira <diegop@pm.me>
2023-09-24 22:54:10 -0300

    refactor: ssl mode is configurable

--- service/mytube/db.go
+++ service/mytube/db.go
@@ -12,7 +12,8 @@ func InitDb() (db *gorm.DB, err error) {
 	password := os.Getenv("POSTGRES_PASSWORD")
 	user := os.Getenv("POSTGRES_USER")
 	host := os.Getenv("POSTGRES_HOST")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=mytube_service port=5432 sslmode=allow", host, user, password)
+	sslMode := os.Getenv("POSTGRES_SSLMODE")
+	dsn := fmt.Sprintf("sslmode=%s host=%s user=%s password=%s dbname=mytube_service port=5432", sslMode, host, user, password)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	return
 }
